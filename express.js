f4655feef3fffabfd7be08c257a39acf7aa5e94c
Caryn McCarthy <cmccarthy15@gmail.com>
2017-09-18 16:54:35 -0400

    server w/o express

--- express.js
+++ express.js
@@ -1,10 +1,28 @@
-var http = require('http');
-var server = http.createServer();
+// WITHOUT EXPRESS
+
+const http = require('http');
+const server = http.createServer();
+
 
 server.on('request', function(req, resp){
-  console.log('got a request');
+  console.log('got a request', req.url);
+
+  if (req.url === '/example' && req.method === "GET"){
+    resp.writeHead(200, {"Content-Type": "text/plain"});
+    resp.end("here's a response for /example");
+  } else if (req.rul === '/' && req.method === "POST") {
+    resp.writeHead(200, {"Content-Type": "text/html"});
+    resp.end("<h1>Here's a response</h1>");
+  }
 });
 
+
+
 server.listen(1337, function(){
-  console.log('listening on port 1337');
+  console.log('listening on port 1337...');
 });
+
+
+
+
+// WITH EXPRESS
